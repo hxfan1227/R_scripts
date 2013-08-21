cd8615cdee2af9b6adec4dade18b7e05b4b0996c
Lilo Henke <lmkh201@exeter.ac.uk>
2013-08-21 21:12:36 +0100

    varclim function completed, ready to check

--- functions/var_clim.R
+++ functions/var_clim.R
@@ -35,7 +35,7 @@ longname<-"Climatology"
 
 
 
-varclim<-function(vb,model,period,dx,dy,conv=86400)
+varclim<-function(vb,model,period,dx,dy)
 {
 	##Get filenames of all data timesets within a model timeslice
 	filename<-paste(vb,"Amon",model,"*",sep="_") #creates search string "[var]_Amon_[model]_*"
@@ -81,7 +81,7 @@ varclim<-function(vb,model,period,dx,dy,conv=86400)
 	filename<-paste(vb,"Amon",model,"*",sep="_") #creates search string "[var]_Amon_[model]_*"
 	files<-Sys.glob(filename) #vector with names of all raw datafiles
 
-	if (length(files)==1)
+	if (length(files)==1) #if there is only 1 raw data timeset
 	{
 		nc<-open.ncdf(data,write=T)
 			var<-get.var.ncdf(nc, vb, start=c(1+((x-1)*lon),1+((y-1)*lat),1), count=c(lon,lat,-1))
@@ -106,7 +106,7 @@ varclim<-function(vb,model,period,dx,dy,conv=86400)
 		{
 			for (j in 1:lat)
 			{
-			clim[i,j,]<-vaggregate(var[i,j,],mm,mean,na.rm=T) 
+				clim[i,j,]<-vaggregate(var[i,j,],mm,mean,na.rm=T) 
 			}
 		}
 		##add data to new netCDF file
@@ -114,7 +114,7 @@ varclim<-function(vb,model,period,dx,dy,conv=86400)
 			put.var.ncdf(nc,climvar,clim,start=c(1,1,1),count=c(-1,-1,12))
 		close.ncdf(nc)
 	}
-	else
+	else #if there are more than 1 raw data timesets that need to be combined to make a final climatology
 	{
 		for (f in 1:length(files))
 		{
@@ -134,156 +134,42 @@ varclim<-function(vb,model,period,dx,dy,conv=86400)
 			{
 				for (j in 1:lat)
 				{
-				clim[i,j,]<-vaggregate(var[i,j,],mm,mean,na.rm=T) 
+					clim[i,j,]<-vaggregate(var[i,j,],mm,mean,na.rm=T) 
 				}
 			}
 			##add data to new netCDF file
-			nc<-open.ncdf(ncname,write=T)
-				put.var.ncdf(nc,climvar,clim,start=c(1,1,(1+((f-1)*12),count=c(-1,-1,12))
+			nc<-open.ncdf(ncname,write=T) 
+				#add each timeset's clim onto time dimension
+				put.var.ncdf(nc,climvar,clim,start=c(1,1,(1+((f-1)*12),count=c(-1,-1,12)) 
 			close.ncdf(nc)
 		}
+		##compute final climatology
+		nc<-open.ncdf(ncname,write=T)
+			#create 4D array to hold clim means for each timeset
+			means<-array(NA,dim=c(lon,lat,12,length(files)))
+			#create array to hold final climatology
+			clim<-array(NA,dim=c(lon,lat,12))
+			#generate levels for each month in each timeset
+			gg<-gl(12, 1, 12*length(files))
+			#loop to put data in means array
+			for (f in 1:length(files))
+			{
+				means[,,,f]<-get.var.ncdf(nc,climvar,start=c(1,1,(1+((f-1)*12),count=c(-1,-1,12))
+			}
+			#take mean over all timesets (4th dimension)
+			for (i in 1:lon)
+			{
+				for (j in 1:lat)
+				{
+					clim[i,j,]<-vaggregate(means[i,j,,],gg,mean)
+				}
+			}
+			##add data to netCDF file. Overwrites old values for first timeset, but other timesets will still be there
+			put.var.ncdf(nc,climvar,clim,count=c(-1,-1,12))
+		close.ncdf(nc)
+
+
+
 	}
-
-
-####this script will calculate the final climatology over the entire timeseries. Repeat this for each lon section!!
-
-ncname<-"CCSM4_pr_piC_clim3.nc"	##name of netCDF file with clims
-units<- "mm/day"		##units of variable
-varclim<-"climatology3"		##name of climatology variable created
-nc<-open.ncdf(ncname)
-long <-nc$dim$lon3		##lon dimension (change lon1/2/3)
-longlen<- nc$dim$lon3$len	##lon dimension length (lon1/2/3)
-close.ncdf(nc)
-nt<-3				##number of timesets in clim netCDF
-
-
-###1. CLIMATOLOGICAL MEAN #########
-nc<-open.ncdf(ncname,write=T)
-#create 4D array to hold clim means for each timeset
-means<-array(NA,dim=c(longlen, nc$dim$lat$len,12,nt))
-#create array to hold final climatology
-clim<-array(NA,dim=c(longlen, nc$dim$lat$len,12))
-#generate levels for each month in each timeset
-gg<-gl(12, 1, 12*nt)
-#loop to put data in means array, then take mean over all timesets (4th dimension)
-for (i in 1:nt)
-{
-means[,,,i]<-get.var.ncdf(nc,nc$var[[i]],count=c(-1,-1,12))
 }
-
-for (j in 1:longlen)
-{
-for (k in 1:nc$dim$lat$len)
-{
-for (l in 1:12)
-clim[j,k,]<-vaggregate(means[j,k,,],gg,mean)
-}}
-
-close.ncdf(nc)
-
-#######2. Add to netCDF file ##################
-##define variable 
-monmean<-var.def.ncdf(varclim,units,list(long,nc$dim$lat,nc$dim$time),NA,longname="Climatology")
-
-##add variable and data
-nc<-open.ncdf(ncname, write=T)
-nc<-var.add.ncdf(nc,monmean)
-
-close.ncdf(nc)
-nc<-open.ncdf(ncname, write=T)
-put.var.ncdf(nc,varclim,clim,count=c(-1,-1,12))
-
-close.ncdf(nc)
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###1. Get precipitation data (tropics) ##############
-
-nc<-open.ncdf(data)
-#define lat indexes for tropics (40S-40N)
-lat.min<-match(nc$dim$lat$vals[round(nc$dim$lat$vals)==-40],nc$dim$lat$vals) 
-lat.max<-match(nc$dim$lat$vals[round(nc$dim$lat$vals)==40],nc$dim$lat$vals) 
-
-lon<-nc$dim$lon$len/set
-lat<-lat.max-lat.min+1
-time<-nc$dim$time$len
-yy<-time/12
-
-
-#get variable data and dimensions for the tropics
-var<-get.var.ncdf(nc, vb, start=c(1+((n-1)*lon),lat.min,1), count=c(lon,lat,-1))
-
-close.ncdf(nc)
-
-#convert to mm/day
-var<-var*conv
-
-#create levels for each month (jan=1,feb=2,...)
-mm<-gl(12, 1, time)
-#create an array to hold monthly means
-clim<-array(NA,dim=c(lon,lat,12)) 
-#change variable dims and loop to calculate means for each month
-
-for (i in 1:lon)
-{
-for (j in 1:lat)
-{
-clim[i,j,]<-vaggregate(var[i,j,],mm,mean,na.rm=T) 
-}}
-
-
-###2. Create ncetCDF file ################################
-##repeat for all timesets if applicable
-
-##FIRST TIME: Define dimensions
-nc<-open.ncdf(data)
-x <- dim.def.ncdf( "lon1", "degrees_east", nc$dim$lon$vals[x.min:x.max])
-y <- dim.def.ncdf( "lat", "degrees_north", nc$dim$lat$vals[lat.min:lat.max])
-t <- dim.def.ncdf( "time", "timesteps", 1:totaltime,unlim=TRUE)
-close.ncdf(nc)
-
-##define variables
-climvar<-var.def.ncdf(varname,units,list(x,y,t),NA,longname=longname)
-
-##create netCDF file
-nc<-create.ncdf(ncname, climvar)
-
-##add data to netCDF file
-put.var.ncdf(nc,varname,clim,start=c(1,1,1),count=c(-1,-1,12))
-
-close.ncdf(nc)
-
-
-
-##SUBSEQUENT TIMES: define variable
-nc<-open.ncdf(ncname,write=T)
-climvar<-var.def.ncdf(varname,units,list(nc$dim$lon1,nc$dim$lat,nc$dim$time),NA,longname=longname)
-nc<-var.add.ncdf(nc,climvar)
-
-close.ncdf(nc)
-
-##add data to netCDF file
-nc<-open.ncdf(ncname,write=T)
-put.var.ncdf(nc,varname,clim,start=c(1,1,1),count=c(-1,-1,12))
-
-close.ncdf(nc)
-
 
